0d8e0cff31997c1863b15626ccbde7a51133c639
yidye <yidnekachewtebeje@gmail.com>
2022-10-04 18:39:27 +0300

    nextgreaterelement

--- NextGreaterElement.cpp
+++ NextGreaterElement.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
+//next greater element
 
 vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
 {
-    vector<int> v;
-    for (int i = 0; i < nums1.size(); i++){
-        int key = nums1[i];
-        int found = 0;
-        for (int j = 0; j < nums2.size(); j++)
-        {
-            if(nums2[j] == key){
-                for (int k = j + 1; j < nums2.size(); j++){
-                    if(nums2[k] > key){
-                        v.push_back(nums2[k]);
-                        found = 1;
-                        break;
-                    }
-                }
+    int v[nums2.size()];
+    stack<int> monoStack;
+    for (int i = nums2.size() - 1; i >=0; i--){
+        
+        while((!monoStack.empty()) && monoStack.top() < nums2[i]){
+            monoStack.pop();
+        }
+        if(monoStack.empty()){
+            v[i] = -1;
+            monoStack.push(nums2[i]);
+        }else{
+            int a = monoStack.top();
+            v[i] = a;
+            monoStack.push(nums2[i]);
+        }
+    }
+    vector<int> nextGreater;
+    nextGreater.reserve(nums1.size() * (sizeof(int)));
+    for (int j = 0; j < nums1.size(); j++)
+    {
+        for (int k = 0; k < nums2.size(); k++){
+            if(nums1[j] == nums2[k]){
+                nextGreater.push_back(v[k]);
                 break;
             }
         }
-        if(found == 0)
-            v.push_back(-1);
     }
-        return v;
+        return nextGreater;
 }
 //next greater integer
 //next integer
